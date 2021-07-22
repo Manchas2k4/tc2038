48855ecd8ffbf194527c1281af4495fa088b7d17
Pedro Perez <pperezm@tec.mx>
2021-07-22 10:24:54 -0500

    8queens and slides week 2 added

---
+++ 03-backtracking/01-8queens/eigth_queen.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+int caseNumber = 0;
+
+bool canPlace(int *rows, int r, int c) {
+  for (int i = 0; i < c; i++) {
+    if ( (rows[i] == r) || (abs(rows[i] - r) == abs(i - c)) ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void eigthQueens(int *rows, int a, int b, int c) {
+  if (c == 8 && rows[b] == a) {
+    std::cout << "Solution " << (++caseNumber) << ": ";
+    for (int i = 0; i < 8; i++) {
+      std::cout << (rows[i] + 1) << " ";
+    }
+    std::cout << "\n";
+  }
+
+  for (int r = 0; r < 8; r++) {
+    if (canPlace(rows, r, c)) {
+      rows[c] = r;
+      eigthQueens(rows, a, b, c + 1);
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  int a, b;
+  int rows[8] = {0};
+
+  std::cin >> a >> b;
+  eigthQueens(rows, a, b, 0);
+}
